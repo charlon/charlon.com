8de508a96d50d842961a08e3f9a237f347bcf976
Charlon Palacay <charlon@gmail.com>
2018-03-29 12:28:33 -0700

    Updates page header.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Charlon.com</h1>
+          <h1 className="App-title">Welcome to Charlon.com - Charlon Palacay's Portfolio</h1>
         </header>
         <p className="App-intro">
           Testing AWS Codestar deployment with React. To get started, edit <code>src/App.js</code> and save to reload.
