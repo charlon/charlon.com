665e729cb12199b1a8aabb1c69696c6cb960c914
Charlon Palacay <charlon@gmail.com>
2018-03-29 13:16:53 -0700

    Adds react routing and base portfolio mockup.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,21 +1,72 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Charlon.com - Charlon Palacay's Portfolio</h1>
-        </header>
-        <p className="App-intro">
-          Testing AWS Codestar deployment with React. To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/portfolio">Portfolio</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/portfolio" component={Portfolio} />
+    </div>
+  </Router>
+);
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+
+const Portfolio = ({ match }) => (
+  <div>
+    <h2>Portfolio</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>Components</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:projectId`} component={Project} />
+    <Route
+      exact
+      path={match.url}
+      render={() => <h3>Please select a project.</h3>}
+    />
+  </div>
+);
+
+const Project = ({ match }) => (
+  <div>
+    <h3>{match.params.projectId}</h3>
+  </div>
+);
 
 export default App;
