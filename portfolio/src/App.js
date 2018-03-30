23cfa4b8d9467e805f61067cf10ca1883e1e8b5b
Charlon Palacay <charlon@gmail.com>
2018-03-30 13:27:33 -0700

    Updates base theme to use Bootstrap example.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,31 +1,106 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Router, Switch, Route, Link } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
 const App = () => (
-  <Router>
-    <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/portfolio">Portfolio</Link></li>
-      </ul>
 
-      <hr />
-
+    <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
       <Route path="/portfolio" component={Portfolio} />
-    </div>
-  </Router>
+    </Switch>
+
 );
 
 const Home = () => (
-  <div>
-    <h2>Home</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel pellentesque nibh, eu euismod mi. In sit amet libero sapien. Fusce erat sapien, egestas fermentum blandit in, luctus in est. Cras a pellentesque urna, at pharetra justo. <Link to="/about">more about me</Link></p>
+
+<div>
+
+  <section class="jumbotron text-center">
+    <div class="container">
+      <h1 class="jumbotron-heading">Welcome to my site...</h1>
+      <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
+      <p>
+        <a href="/about" class="btn btn-primary my-2">more about me</a>
+
+      </p>
+    </div>
+  </section>
+
+  <div class="album py-5 bg-light">
+    <div class="container">
+      <div class="row">
+
+        <h3 class="col-md-12">Featured work</h3>
+
+        <div class="col-md-4">
+          <div class="card mb-4 box-shadow">
+            <img class="card-img-top" src="http://via.placeholder.com/225x100" alt="Card image cap" />
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 box-shadow">
+            <img class="card-img-top" src="http://via.placeholder.com/225x100" alt="Card image cap" />
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 box-shadow">
+            <img class="card-img-top" src="http://via.placeholder.com/225x100" alt="Card image cap" />
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 box-shadow">
+            <img class="card-img-top" src="http://via.placeholder.com/225x100" alt="Card image cap" />
+            <div class="card-body">
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-12">
+          <a href="/portfolio" class="btn btn-secondary my-2">view portfolio</a>
+        </div>
+
+      </div>
+    </div>
   </div>
+
+</div>
+
 );
 
 const About = () => (
