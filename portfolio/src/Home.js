2971bacfc26926ac6ab0dfa5b241166c416e6929
Charlon Palacay <charlon@gmail.com>
2018-04-01 22:41:26 -0700

    Full page cover scrolling.

--- portfolio/src/Home.js
+++ portfolio/src/Home.js
@@ -4,87 +4,99 @@ import './Home.css'
 const Home = () => (
   <div>
 
-    <header class="text-center home-me">
-      <div class="container">
-        <div class="row">
-
-          <div class="home-me-content">
-              <div class="home-me-content-inner">
+    <header class="text-center bg-light home-me">
+      <div class="home-content">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
               <h1 class="">Charlon Palacay</h1>
               <p class="lead text-muted">UX Designer + Front-end Developer</p>
             </div>
           </div>
-
         </div>
       </div>
     </header>
 
-    <section class="jumbotron text-center">
-      <div class="container">
-        <h2 class="jumbotron-heading">Who Am I</h2>
-        <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
-        <p><a href="/about" class="btn btn-primary my-2">more about me</a></p>
-      </div>
-    </section>
-
-    <section class="jumbotron text-center bg-light">
-      <div class="container">
-        <div class="row">
-
-          <h2 class="col-md-12 jumbotron-heading">What I do</h2>
-          <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
-
-          <div class="col-md-6">
-            <div class="card mb-6 box-shadow">
-              <div class="card-body">
-                <h3>Research</h3>
-                <p class="card-text">Understand the problem, empathize with the person. Explore the competitive landscape. Identify strategic opportunities. Define design goals and requirements.</p>
-              </div>
+    <section class="text-center">
+      <div class="home-content">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <h2>Who Am I</h2>
+              <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
+              <p><a href="/about" class="btn btn-primary my-2">more about me</a></p>
             </div>
           </div>
-          <div class="col-md-6">
-            <div class="card mb-6 box-shadow">
-              <div class="card-body">
-                <h3>UX Design</h3>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="card mb-6 box-shadow">
-              <div class="card-body">
-                <h3>Product Management</h3>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-6">
-            <div class="card mb-6 box-shadow">
-              <div class="card-body">
-                <h3>Front-end Develop</h3>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-12">
-            <a href="/portfolio" class="btn btn-secondary my-2">view portfolio</a>
-          </div>
-
         </div>
       </div>
-
     </section>
 
-    <section class="jumbotron text-center home-uw">
-      <div class="container">
-        <h2 class="jumbotron-heading">Where I work</h2>
-        <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
-        <p>
-          <a href="/resume" class="btn btn-primary my-2">view my resume</a>
+    <section class="text-center bg-light">
+      <div class="home-content">
+        <div class="container">
+          <div class="row">
 
-        </p>
+            <div class="col-md-12">
+              <h2>What I do</h2>
+              <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
+            </div>
+
+            <div class="col-md-3">
+              <div class="card mb-3 box-shadow">
+                <div class="card-body">
+                  <h3>Research</h3>
+                  <p class="card-text">Understand the problem, empathize with the person. Explore the competitive landscape. Identify strategic opportunities. Define design goals and requirements.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="card mb-3 box-shadow">
+                <div class="card-body">
+                  <h3>UX Design</h3>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-3">
+              <div class="card mb-3 box-shadow">
+                <div class="card-body">
+                  <h3>Product Management</h3>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-3">
+              <div class="card mb-3 box-shadow">
+                <div class="card-body">
+                  <h3>Front-end Develop</h3>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-12">
+              <a href="/portfolio" class="btn btn-primary my-2">view my portfolio</a>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="text-center home-uw">
+      <div class="home-content">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+
+              <h2 class="">Where I work</h2>
+              <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
+              <p><a href="/resume" class="btn btn-primary my-2">view my resume</a></p>
+
+            </div>
+          </div>
+        </div>
       </div>
     </section>
 
