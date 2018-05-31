e599cb837a93ba8be9507bc907af2e005e12399f
Charlon Palacay <charlon@gmail.com>
2018-05-30 21:28:59 -0700

    Updates home and resume pdf.

--- portfolio/src/containers/Home.js
+++ portfolio/src/containers/Home.js
@@ -15,7 +15,7 @@ const Home = () => (
           <div className="row">
             <div className="col-md-6 ooops">
               <h1 className="sr-only">Charlon Palacay</h1>
-              <p className="lead">Hello! I'm <strong>Charlon Palacay</strong>, a product designer and front-end developer from Seattle, Washington. I have a passion for creating and designing positive user experiences.</p>
+              <p className="lead">I am <strong>Charlon Palacay</strong>, a user experience and front-end developer from Seattle, Washington. I am passionate about creating positive experiences for people on the web.</p>
               <p><a href="/portfolio" className="btn btn-outline-secondary">view portfolio</a></p>
             </div>
             <div className="col-md-6 blah">
@@ -32,43 +32,34 @@ const Home = () => (
           <div className="row">
 
             <div className="offset-md-2 col-md-8 text-center">
-              <h2>Product Designer</h2>
-              <p className="lead">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
+              <h2>Expertise</h2>
+              <p className="lead">I have over 15 years of professional experience building and designing intuitive user interfaces.</p>
             </div>
 
-            <div class="w-100 mb-5"></div>
+            <div className="w-100 mb-5"></div>
 
             {/* hide on mobile only */}
-            <div className="col-md-3 d-none d-sm-block">
-              <div className="card mb-3 box-shadow">
+            <div className="col-md-4 d-none d-sm-block">
+              <div className="card mb-4 box-shadow">
                 <div className="card-body">
-                  <h3>User Research</h3>
+                  <h3>User Experience</h3>
                   <p className="card-text">Understand the problem, empathize with the person. Explore the competitive landscape. Identify strategic opportunities. Define design goals and requirements.</p>
                 </div>
               </div>
             </div>
-            <div className="col-md-3 d-none d-sm-block">
-              <div className="card mb-3 box-shadow">
+            <div className="col-md-4 d-none d-sm-block">
+              <div className="card mb-4 box-shadow">
                 <div className="card-body">
-                  <h3>Content Strategy</h3>
+                  <h3>Product Design</h3>
                   <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                 </div>
               </div>
             </div>
-            <div className="col-md-3 d-none d-sm-block">
-              <div className="card mb-3 box-shadow">
+            <div className="col-md-4 d-none d-sm-block">
+              <div className="card mb-4 box-shadow">
                 <div className="card-body">
-                  <h3>Visual Design</h3>
-                  <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-md-3 d-none d-sm-block">
-              <div className="card mb-3 box-shadow">
-                <div className="card-body">
-                  <h3>Front-end Development</h3>
-                  <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <h3>Web Development</h3>
+                  <p className="card-text">HTML/CSS and Javascript. This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                 </div>
               </div>
             </div>
@@ -89,8 +80,8 @@ const Home = () => (
 
             <div className="col-md-6">
               <h2 className="">Featured Work</h2>
-              <p className="lead">I've worked in higher-education over the past 8 years improving the student academic experience by designing thoughtful and simple interfaces. Meet Scout!</p>
-              <p><a href="/portfolio/scout" className="btn btn-outline-secondary">read case study</a></p>
+              <p className="lead">I've worked in higher-education over the past 8 years improving the student academic experience by designing thoughtful and simple interfaces.</p>
+              <p><a href="https://scout.uw.edu" className="btn btn-outline-secondary">Meet Scout!</a></p>
             </div>
 
             <div className="col-md-12">
